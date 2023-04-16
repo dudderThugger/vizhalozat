8efe03985fbc8255bdad19a3caa84df41f3958af
SmokeDaReal <szmok.peter1@gmail.com>
2023-04-16 22:11:57 +0200

    teszteseteim kommentelése

--- src/Vizhalozat/Szkeleton.java
+++ src/Vizhalozat/Szkeleton.java
@@ -639,6 +639,7 @@ public class Szkeleton {
 
     public void teszt21(){
         System.out.println("21.Teszt: Játékos csőre lép");
+        /** Objektumok létrehozása */
         Jatek j = new Jatek(this);
         Pumpa rajtaAll = new Pumpa( j,this);
         Szerelo sz = new Szerelo(rajtaAll ,this);
@@ -658,6 +659,7 @@ public class Szkeleton {
 
     public void teszt22(){
         System.out.println("22.Teszt: Játékos AktívElemre lép");
+        /** Objektumok létrehozása */
         Jatek j = new Jatek(this);
         Cso rajtaAll = new Cso( j,this);
         Szerelo sz = new Szerelo(rajtaAll ,this);
@@ -677,7 +679,7 @@ public class Szkeleton {
 
     public void teszt23(){
         System.out.println("23.Teszt: Játék indítása, main meghívása");
-
+        /** Objektumok létrehozása */
         App a = new App();
 
         ids.put(a, "a");
@@ -691,6 +693,7 @@ public class Szkeleton {
 
     public void teszt24(){
         System.out.println("24.Teszt: Csövek generálása ciszternáknál");
+        /** Objektumok létrehozása */
         Jatek j = new Jatek(this);
         Ciszterna c = new Ciszterna(j, this);
         ids.put(c, "c");
