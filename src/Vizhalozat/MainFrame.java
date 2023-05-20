e54f3cb2ba1ac2cb9004efea4b020fa2714698fa
dudderThugger <88734852+dudderThugger@users.noreply.github.com>
2023-05-20 23:09:53 +0200

    kevés gui

---
+++ src/Vizhalozat/MainFrame.java
@@ -0,0 +1,15 @@
+package Vizhalozat;
+
+import javax.swing.*;
+
+public class MainFrame {
+    private JPanel panel1;
+    private JButton jatekInditasaButton;
+    private JButton kilépésButton;
+    private JPanel menuPanel;
+    private JPanel jatekPanel;
+    private JPanel jatekVegePanel;
+    private JButton ujJatek;
+    private JButton kilepesButton;
+    private JPanel allapotPanel;
+}
