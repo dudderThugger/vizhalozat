99e571f35d77294c4d47124c1536a882b00abfed
adamkaj <adam2000buzas@gmail.com>
2023-05-29 21:16:37 +0200

    csövek

--- src/Vizhalozat/CsoMegfigyelo.java
+++ src/Vizhalozat/CsoMegfigyelo.java
@@ -1,6 +1,10 @@
 package Vizhalozat;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class CsoMegfigyelo extends Megfigyelo {
     public CsoMegfigyelo(Cso cso, Point hova) {
@@ -9,7 +13,20 @@ public class CsoMegfigyelo extends Megfigyelo {
 
     @Override
     public void draw(Graphics g) {
-
+        try {
+            if(!selected) {
+                File file = new File("src/images/cso_sima.png");
+                BufferedImage img = ImageIO.read(file);
+                g.drawImage(img, coordinates.x, coordinates.y, 50, 50, null);
+            }
+            else{
+                File file = new File("src/images/cso_kijelolt.png");
+                BufferedImage img = ImageIO.read(file);
+                g.drawImage(img, coordinates.x, coordinates.y, 50, 50, null);
+            }
+        } catch(IOException e) {
+            System.out.println("File not found exception!");
+        }
     }
 
     @Override
