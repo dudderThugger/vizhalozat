8f97eea311e6c54e36226182e4f4087ef57ec918
kissPeterOliver <kisspeteroliver@gmail.com>
2023-05-30 21:57:17 +0200

    Vezerlo csogeneralas es kommentek

--- src/GUI/Vezerlo.java
+++ src/GUI/Vezerlo.java
@@ -5,7 +5,9 @@ import Vizhalozat.*;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
-
+/**
+ * A játék megejelítését vezérlő osztály
+ */
 public class Vezerlo {
     private Jatek jatek;
     ArrayList<Mezo> mezok = new ArrayList<>();
@@ -24,7 +26,11 @@ public class Vezerlo {
     private int korIdo;
     private Cso selected;
     private JatekPanel panel;
+    private int ciszternaCounter = 0;
 
+    /**
+     * Akcio tipusok
+     */
     enum Akcio {
         LEP,
         LYUKASZT,
@@ -39,6 +45,14 @@ public class Vezerlo {
         CSOFELVEVES
     }
 
+    /**
+     *
+     * Vezérlő osztály konstruktora
+     * @param szerelo1name 1. szerelo neve
+     * @param szerelo2name 2. szerelo neve
+     * @param szabotor1name 1. szabotor neve
+     * @param szabotor2name 2. szabotor neve
+     */
     public Vezerlo(String szerelo1name, String szerelo2name, String szabotor1name, String szabotor2name) {
         jatekosNevek.add(szerelo1name); jatekosNevek.add(szerelo2name); jatekosNevek.add(szabotor1name); jatekosNevek.add(szabotor2name);
         jatekosNevek.add(szerelo2name);
@@ -56,11 +70,17 @@ public class Vezerlo {
         selected = null;
         korIdo = 15;
     }
+
     public void init(){
         jatek.init();
     }
 
     public void panel(JatekPanel p){panel=p;}
+
+    /**
+     * Egér kattintásra reeagáló függvény
+     * @param mezo A mező amire kattintunk
+     */
     public void kattintas(Mezo mezo) {
         Jatekos jatekos = jatekosok.get(actualJatekosIndex);
         System.out.println(akcio + " " + mezo);
@@ -93,6 +113,10 @@ public class Vezerlo {
         }
     }
 
+    /**
+     * Gomblenyomásra reagáló függvény
+     * @param akcio Az akció amit kiválasztottunk
+     */
     public void gombLenyomas(Akcio akcio) {
         Jatekos jatekos = jatekosok.get(actualJatekosIndex);
         selected = null;
@@ -149,17 +173,24 @@ public class Vezerlo {
         }
     }
 
+    /**
+     * A másodpercenkénti köridő számlálást végző függvény
+     */
     public void tickeles() {
         --korIdo;
         if(korIdo >= 0) {
             jatek.tick();
         } else {
             jatekosValtas();
+            csoGeneralas();
         }
         int i = 0;
         panel.frissit(jatekosNevek.get(actualJatekosIndex),korIdo,actualJatekosIndex,i);
     }
 
+    /**
+     * Aktuális játékos váltását végző metódus
+     */
     public void jatekosValtas() {
         korIdo = 15;
         if(actualJatekosIndex == 3) { actualJatekosIndex = 0; }
@@ -169,51 +200,102 @@ public class Vezerlo {
         jatek.tick();
     }
 
+    /**
+     * Egy cső generálása egy ciszternán
+     */
+    public void csoGeneralas(){
+        Cso ujCso = new Cso(this.jatek);
+        mezok.add(ujCso);
+        csovek.add(ujCso);
+
+        if(ciszternaCounter >= ciszternak.size()) ciszternaCounter = 0;
+        ujCso.addSzomszed(ciszternak.get(ciszternaCounter));
+        panel.addElemMegfigyelo(new GUI.CsoMegfigyelo(ujCso, panel));
+        System.out.println("cső létrehozva" + ciszternak.get(ciszternaCounter));
+        ciszternaCounter++;
+    }
+
+    /**
+     * Játék végét hívó függvény
+     * @param nyertesCsapat A nyertes
+     */
     public void jatekVege(String nyertesCsapat) {
         panel.jatekVege(nyertesCsapat);
     }
 
+    /**
+     * Egy csövet ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param cso A hozzáadni kívánt cső
+     */
     public void addCso(Cso cso) {
         mezok.add(cso);
         csovek.add(cso);
         panel.addElemMegfigyelo(new GUI.CsoMegfigyelo(cso, panel));
     }
-
+    /**
+     * Egy pumpát ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param pumpa A hozzáadni kívánt pumpa
+     * @param hova A pumpa koordinátái
+     */
     public void addPumpa(Pumpa pumpa, Point hova) {
         mezok.add(pumpa);
         pumpak.add(pumpa);
         panel.addElemMegfigyelo(new GUI.PumpaMegfigyelo(pumpa, hova, panel));
     }
-
+    /**
+     * Egy forrást ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param forras A hozzáadni kívánt forrás
+     * @param hova A forrás koordinátái
+     */
     public void addForras(Forras forras, Point hova) {
         mezok.add(forras);
         forrasok.add(forras);
         panel.addElemMegfigyelo(new GUI.ForrasMegfigyelo(forras, hova, panel));
     }
-
+    /**
+     * Egy ciszternát ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param ciszterna A hozzáadni kívánt ciszterna
+     * @param hova A ciszterna koordinátái
+     */
     public void addCiszterna(Ciszterna ciszterna, Point hova) {
         mezok.add(ciszterna);
         ciszternak.add(ciszterna);
         panel.addElemMegfigyelo(new GUI.CiszternaMegfigyelo(ciszterna, hova, panel));
     }
-
+    /**
+     * Egy szerelőt ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param szerelo A hozzáadni kívánt szerelő
+     */
     public void addSzerelo(Szerelo szerelo) {
         jatekosok.add(szerelo);
         szerelok.add(szerelo);
         panel.addSzereloMegfigyelo(new GUI.SzereloMegfigyelo(szerelo, panel));
     }
-
+    /**
+     * Egy szabotőrt ad hozzá a vezérlőhöz és a megfigyelőkhöz
+     * @param szabotor A hozzáadni kívánt szabotőr
+     */
     public void addSzabotor(Szabotor szabotor) {
         jatekosok.add(szabotor);
         szabotorok.add(szabotor);
         panel.addSzabotorMegfigyelok(new GUI.SzabotorMegfigyelo(szabotor, panel));
     }
 
+    /**
+     * Visszaadja a várakozási időt
+     * @return várakozási idő
+     */
     public int getCooldown() {
         return jatekosok.get(actualJatekosIndex).getAkcioIdo();
     }
-
+    /**
+     * Visszaadja a maradék ragasztási időt időt
+     * @return maradék ragasztási idő
+     */
     public int getStickTime() {
         return jatekosok.get(actualJatekosIndex).getRagadasiIdo();
     }
+
+
+
 }
